32da5e36129f12d53770fd71937a5c6cb22016d3
Angel_Delgado <al02959951@tecmilenio.mx>
2023-03-11 03:05:14 -0500

    Haciendo el factory

---
+++ database/factories/cursosFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\cursos;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\cursos>
+ */
+class cursosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'cursos' -> $this -> faker -> words(3, true),
+            'portada' -> $this -> faker -> sentence(3),
+            'contenido' -> $this -> faker -> paragraph(2),
+            'materialDidactico' -> $this -> faker -> words(2, true),
+        ];
+    }
+}
--- database/seeders/cursosSeeder.php
+++ database/seeders/cursosSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\cursos;
 
 class cursosSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class cursosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // DB::table('cursos') -> insert([
+
+        //     ['cursos' => 'Introduction to robotics', 'portada' => 'Introduction to robotics', 'contenido' => 'Un curso introductorio de robotica', 'materialDidactico' => 'Starterkit'],
+        //     ['cursos' => 'Introduction to automation', 'portada' => 'Introduction to automation', 'contenido' => 'Un curso introductorio de automatizacion', 'materialDidactico' => 'Starterkit'],
+        //     ['cursos' => 'Programming for robotics', 'portada' => 'Programming robots', 'contenido' => 'Un curso de programacion para robotica', 'materialDidactico' => 'Educational Robotics Kit'],
+        //     ['cursos' => 'Characteristics of a robot ', 'portada' => 'Más sobre robots', 'contenido' => 'Un curso sobre robotica', 'materialDidactico' => 'kit5'],
+
+        // ]);
+
+
+            cursos::factory() -> count(100) -> create();
+
+
     }
 }
