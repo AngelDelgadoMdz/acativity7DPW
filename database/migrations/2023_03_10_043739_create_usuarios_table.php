98c1e04b6a9c393fdc763e1206d34ef658c9dc65
Angel_Delgado <al02959951@tecmilenio.mx>
2023-03-10 22:54:42 -0500

    Cambios en la base de datos

--- database/migrations/2023_03_10_043739_create_usuarios_table.php
+++ database/migrations/2023_03_10_043739_create_usuarios_table.php
@@ -14,11 +14,17 @@ return new class extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
 
+            $table -> string('nombreUsuario');
+            $table -> string('email');
+            $table -> string('password');/**Contraseña */
+
+
             $table -> string('rol'); /**Agrego el atributo de rol para la tabla usuarios */
             /**Considero que es complicado tener una tabla solo de usuarios 
              * puesto a que cada usuario debe tener un id correspondiente en 
              * sus propias tablas las cuales pueden funcionar mal en conjunto */
             /**Solo que no supe como borrar esta tabla :'( */
+            /**Ya vi que para la tarea no es redundante esta tabla */
             $table->timestamps();
         });
     }
